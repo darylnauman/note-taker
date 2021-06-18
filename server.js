b6b7b0b376b7a36f3ad0522d2ba100ecc3dfbfee
Daryl Nauman <darylnauman@gmail.com>
2021-06-17 21:16:37 -0400

    created initial server.js file with basic info

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+// Set up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// --- Routes ---
+
+// HTML Routes
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+// API Routes
+// TO DO
+// - GET /api/notes should read the db.json file and return all saved notes as JSON.
+// - POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+
+// Starts the server to begin listening
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
